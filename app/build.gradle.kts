fc3e6167c678db92fef5aab2453d61e183e79f38
spundev <spundev@gmail.com>
2025-11-09 17:40:36 +0100

    Update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.spundev.dynamicthemeexport"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.3"
+        versionCode = 4
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
