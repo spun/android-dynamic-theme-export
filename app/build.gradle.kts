7f6de02fba56c291c54958bbed872543f374fdd1
spundev <spundev@gmail.com>
2026-01-15 21:33:16 +0100

    Update to AGP9 - Remove org.jetbrains.kotlin.android plugin. AGP has Kotlin support built in now.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
